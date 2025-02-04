8861cb7ec83a75414d50c4da26f5051ec4067048
Faruq <faruq5840@gmail.com>
2025-02-04 22:03:16 +0100

    Added exit safety with two methond dialog and double tab to exit

--- lib/Screens/main_activity.dart
+++ lib/Screens/main_activity.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:storysaver/Constants/CustomColors.dart';
 import 'package:storysaver/Provider/bottom_nav_provider.dart';
 import 'package:storysaver/Provider/getStatusProvider.dart';
 import 'package:storysaver/Provider/savedMediaProvider.dart';
@@ -7,6 +9,7 @@ import 'package:storysaver/Screens/BottomNavPages/Experiments/ExperimentWithPhot
 import 'package:storysaver/Screens/BottomNavPages/Experiments/ThumbnailExperiment.dart';
 import 'package:storysaver/Screens/BottomNavPages/Images/image.dart';
 import 'package:storysaver/Screens/BottomNavPages/Video/video.dart';
+import 'package:double_tap_to_exit/double_tap_to_exit.dart';
 
 class MainActivity extends StatefulWidget {
   const MainActivity({Key? key}) : super(key: key);
@@ -24,7 +27,7 @@ class _MainActivityState extends State<MainActivity> with SingleTickerProviderSt
     super.initState();
     
 
-    print('init myMainActivity');
+    // print('init myMainActivity');
 
     Provider.of<GetStatusProvider>(context, listen: false).getAllStatus();
 
@@ -48,32 +51,68 @@ class _MainActivityState extends State<MainActivity> with SingleTickerProviderSt
 
   List<Widget> pages = const [ImageHomePage(), VideoHomePage(), MediaStoreVideos()];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Story Saver"),
-        bottom: TabBar(
-          controller: controller,
-          indicatorColor: Colors.white,
-          indicatorSize: TabBarIndicatorSize.tab,
-          tabs:
-          [
-            Tab(child: Text('Image', style: TextStyle(fontSize: 14, color: Colors.white),),),
-            Tab(child: Text('Video', style: TextStyle(fontSize: 14, color: Colors.white),),),
-            Tab(child: Text('Gallery', style: TextStyle(fontSize: 14, color: Colors.white),),),
-          ]
-        ),
+  Future<bool?> _showExitDialog(BuildContext context) async {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Exit App"),
+        content: Text("Are you sure you want to leave?"),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.help_outline_sharp, color: Colors.white)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.share, color: Colors.white)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert, color: Colors.white)),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false), // Stay in app
+            child: Text("No"),
+          ),
+          TextButton(
+            onPressed: () => SystemChannels.platform.invokeMethod('SystemNavigator.pop'), // Exit app
+            child: Text("Yes"),
+          ),
         ],
-        backgroundColor: const Color(0xff154734),
-        foregroundColor: Colors.white,
       ),
-      body: TabBarView(controller: controller, children: pages),
+    );
+  }
 
+
+  @override
+  Widget build(BuildContext context) {  //WillPopScope
+    return  DoubleTapToExit(
+      child: PopScope(
+        canPop: false, // Prevents app from closing automatically
+        onPopInvoked: (didPop) async {
+          if (didPop) return;
+      
+          bool exitApp = await _showExitDialog(context) ?? false;
+          if (exitApp) {
+            Navigator.of(context).pop();
+          }
+        },
+      
+      
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Story Saver"),
+            bottom: TabBar(
+              controller: controller,
+              indicatorColor: Colors.white,
+              indicatorSize: TabBarIndicatorSize.tab,
+              tabs:
+              [
+                Tab(child: Text('Image', style: TextStyle(fontSize: 14, color: Colors.white),),),
+                Tab(child: Text('Video', style: TextStyle(fontSize: 14, color: Colors.white),),),
+                Tab(child: Text('Gallery', style: TextStyle(fontSize: 14, color: Colors.white),),),
+              ]
+            ),
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.help_outline_sharp, color: Colors.white)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.share, color: Colors.white)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.more_vert, color: Colors.white)),
+            ],
+            backgroundColor: const Color(CustomColors.AppBarColor),
+            foregroundColor: Colors.white,
+          ),
+          body: TabBarView(controller: controller, children: pages),
+        
+        ),
+      ),
     );
 
     //   Consumer<BottomNavProvider>(builder: (context, nav, child) {
