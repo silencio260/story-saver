2107ff9c1ff8bc0b1f08f5f505b1ba3c0ee2d1e5
Faruq <faruq5840@gmail.com>
2025-04-01 00:06:50 +0100

    clean up image.dart file

--- lib/Screens/BottomNavPages/Images/image.dart
+++ lib/Screens/BottomNavPages/Images/image.dart
@@ -1,15 +1,9 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 import 'package:storysaver/Provider/getStatusProvider.dart';
-import 'package:storysaver/Screens/BottomNavPages/Images/Image_view.dart';
-import 'package:storysaver/Utils/SavedMediaManager.dart';
 import 'package:storysaver/Utils/getStoragePermission.dart';
 import 'package:storysaver/Widget/GrantPermissionButton.dart';
-import 'package:storysaver/Widget/LocalCachedImage.dart';
 import 'package:storysaver/Widget/MediaListItem.dart';
 import 'package:storysaver/Widget/MyRouteObserver.dart';
 
@@ -38,7 +32,6 @@ class _ImageHomePageState extends State<ImageHomePage>
       });
     });
 
-    // Provider.of<GetStatusProvider>(context, listen: false).getStatus('.jpg');
   }
 
   bool _isFetched = false;
@@ -49,8 +42,6 @@ class _ImageHomePageState extends State<ImageHomePage>
       RefreshController(initialRefresh: false);
 
   void _onRefresh() async {
-    // Provider.of<GetStatusProvider>(context, listen: false).getStatus('.jpg');
-    // Provider.of<GetStatusProvider>(context, listen: false).getStatus('.mp4');
 
     Provider.of<GetStatusProvider>(context, listen: false).getAllStatus();
 
@@ -64,11 +55,7 @@ class _ImageHomePageState extends State<ImageHomePage>
   void didChangeDependencies() {
     super.didChangeDependencies();
     final route = ModalRoute.of(context);
-    // if (route is PageRoute && routeObserver.) {
-    //   print('Subscribing');
-    //   routeObserver.subscribe(this, route);
-    // }
-    // print('Subscribing2');
+
   }
 
   @override
@@ -77,12 +64,6 @@ class _ImageHomePageState extends State<ImageHomePage>
     print('unSubscribing');
     super.dispose();
   }
-  //
-  // @override
-  // void didPopNext() {
-  //   // Called when the user navigates back to this page
-  //   print('Navigated back to SecondPage');
-  // }
 
 
   @override
@@ -149,85 +130,8 @@ class _ImageHomePageState extends State<ImageHomePage>
                               return MediaListItem(
                                 // key: mediaListItemKey,
                                 // key: ValueKey(index),
-                                mediaPath: data.path, currentIndex: index,);
-
-
-                              // return FutureBuilder<bool>(
-                              //     future: mediaManager.isMediaSaved(data.path),
-                              //   builder: (context, snapshot) {
-                              //
-                              //       // bool isAlreadySaved = snapshot.data!;
-                              //
-                              //     return GestureDetector(
-                              //         onTap: () {
-                              //           Navigator.push(
-                              //             context,
-                              //             CupertinoPageRoute(
-                              //               builder: (_) =>
-                              //                   ImageView(imagePath: data.path),
-                              //             ),
-                              //           );
-                              //
-                              //           // print('--+++--- ' +
-                              //           //     data.toString());
-                              //           // print(
-                              //           //     data);
-                              //           // print('--+++--- ' +
-                              //           //     data.path);
-                              //           // print(
-                              //           //     '--+++ ' + snapshot.data.toString());
-                              //         },
-                              //         child: Stack(
-                              //           children: [
-                              //             Positioned.fill(
-                              //               child: LocalImageCache(
-                              //                 data.path, // Cached image widget
-                              //               ),
-                              //             ),
-                              //             Positioned(
-                              //               // top: 10, // Adjust the position as needed
-                              //               right: 10,
-                              //               bottom: 10,
-                              //               child: GestureDetector(
-                              //                 onTap: () async {
-                              //                   // Handle the click event here
-                              //                   print("Icon tapped!");
-                              //                   final result = await mediaManager.saveMedia(data.path);
-                              //
-                              //                   // isAlreadySaved = true;
-                              //                   print('Aready Saved');
-                              //
-                              //                 },
-                              //                 child: Container(
-                              //                   // color: isAlreadySaved ? Colors.green : Colors.grey,
-                              //                   width: 40,
-                              //                   height: 40,
-                              //                   child: Icon(
-                              //                     Icons.download, // Replace with your desired icon
-                              //                     color: const Color.fromARGB(255, 236, 235, 230),
-                              //                     size: 20,
-                              //                   ),
-                              //                 ),
-                              //
-                              //               ),
-                              //             ),
-                              //           ],
-                              //         ),
-                              //
-                              //
-                              //       // Container(
-                              //         //   decoration: BoxDecoration(
-                              //         //       image: DecorationImage(
-                              //         //         image: FileImage(File(data.path)),
-                              //         //         fit: BoxFit.cover,
-                              //         //       ),
-                              //         //       color: const Color.fromARGB(
-                              //         //           255, 255, 255, 255),
-                              //         //       borderRadius: BorderRadius.circular(2)),
-                              //         // ),
-                              //         );
-                              //   }
-                              // );
+                                mediaPath: data.path, currentIndex: index,
+                              );
                             },
                           ),
                         ),
