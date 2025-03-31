ad71533ee64d0713fedc26975b2388599dc9cfbc
Faruq <faruq5840@gmail.com>
2025-03-31 01:11:25 +0100

    clean up getStatusProvider.dart.dart file

--- lib/Provider/getStatusProvider.dart
+++ lib/Provider/getStatusProvider.dart
@@ -143,22 +143,6 @@ class GetStatusProvider extends ChangeNotifier {
     }
   }
 
-  // // Request storage permission
-  // Future<bool> getStoragePermission() async {
-  //   final status = await Permission.storage.request();
-  //   if (status.isGranted) {
-  //     return true;
-  //   } else {
-  //     final storagePermission = await Permission.manageExternalStorage.request();
-  //     if (storagePermission.isGranted) {
-  //       return true;
-  //     } else {
-  //       openAppSettings(); // Optionally prompt user to open settings for manual permission
-  //       return false;
-  //     }
-  //   }
-  // }
-
   // Fetch files from another directory for experimental use
   void getExperimentalStatus(String ext) async {
     if (await getStoragePermission() == true) {
