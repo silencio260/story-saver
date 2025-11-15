fdf0fcb70346c40ba945e061fb8ec670774c8cf7
Faruq <faruq5840@gmail.com>
2025-11-15 01:36:01 +0100

    Implement util to check dev mode

---
+++ lib/Utils/checkDevelopmentMode.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/foundation.dart';
+
+class DevelopmentModeUtils {
+  static bool checkDevelopmentMode() {
+    bool founders_version = const bool.fromEnvironment("founders_version");
+    bool development_mode = const bool.fromEnvironment("development_mode");
+    bool special_version_mode =
+        const bool.fromEnvironment("special_version_mode");
+
+    if (kDebugMode ||
+        founders_version ||
+        development_mode ||
+        special_version_mode) {
+      return true;
+    }
+
+    print(
+        "kDebugMode -> ${kDebugMode}, founders_version -> ${founders_version}, development_mode -> ${development_mode}, special_version_mode -> ${special_version_mode}");
+
+    return false;
+  }
+}
