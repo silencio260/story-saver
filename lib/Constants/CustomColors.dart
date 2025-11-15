ac1d1cefc604fea301c6d1f30ba72190386930d9
Faruq <faruq5840@gmail.com>
2025-11-15 01:38:38 +0100

    feat: Add SideBarColor to CustomColors

--- lib/Constants/CustomColors.dart
+++ lib/Constants/CustomColors.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 
 class CustomColors {
-  static const int AppBarColor = 0xff154734;
+  static const int AppBarColor = 0xff154734; //0xff000000;
   static const int ButtonColor = 0xff154734;
+
+  static const int SideBarColor = 0xff154734;
 }
