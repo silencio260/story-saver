6afdd19674ce3d6871eb91f7a45e1d88c48287a2
Faruq <faruq5840@gmail.com>
2025-11-15 01:39:03 +0100

    feat: Add WhatsApp and WhatsApp Business SVG icons

---
+++ lib/Widget/svgIcons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../Screens/TopNavPages/Widget/SideBar.dart' as Colors;
+
+final Widget whatsAppsSvgIcon = SvgPicture.asset(
+  "assets/icons/whatsapp.svg",
+  // colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+);
+
+final Widget businessWhatsAppsSvgIcon = SvgPicture.asset(
+  "assets/icons/whatsapp-business.svg",
+  // colorFilter: const ColorFilter.mode(Colors.w, BlendMode.srcIn),
+);
