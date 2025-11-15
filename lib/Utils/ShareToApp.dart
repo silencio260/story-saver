818d48407bb19463c3d8e01569191d1f74072044
Faruq <faruq5840@gmail.com>
2025-11-15 01:33:51 +0100

    Implement shareAppLink

--- lib/Utils/ShareToApp.dart
+++ lib/Utils/ShareToApp.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:storysaver/Constants/constant.dart';
+import 'package:storysaver/Services/analytics_service.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-void shareToWhatsApp(String message, {String? filePath, required BuildContext context}) async {
-  final whatsappUrl = Uri.parse("whatsapp://send?text=${Uri.encodeComponent(message)}");
+void shareAppLink(BuildContext context) {
+  AnalyticsService.logShareApp();
+
+  Share.share(
+          'Shared From WhatsApp Status Saver App @ ${AppConstants().GOOGLE_PLAY_STORE_LINK}')
+      .then((value) {
+    // ScaffoldMessenger.of(context)
+    //     .showSnackBar(const SnackBar(content: Text("Image Sent")));
+  });
+}
+
+void shareToWhatsApp(String message,
+    {String? filePath, required BuildContext context}) async {
+  final whatsappUrl =
+      Uri.parse("whatsapp://send?text=${Uri.encodeComponent(message)}");
 
   if (await canLaunchUrl(whatsappUrl)) {
     print('Launching WhatsApp');
