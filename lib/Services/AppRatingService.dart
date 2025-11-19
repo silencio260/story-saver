d6df3af6776ab21f8fb5f28c9e8cad261787b634
Faruq <faruq5840@gmail.com>
2025-11-19 13:02:20 +0100

    Refactor: Remove unused import

--- lib/Services/AppRatingService.dart
+++ lib/Services/AppRatingService.dart
@@ -13,7 +13,6 @@
 //   }
 // }
 
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:in_app_review/in_app_review.dart';
